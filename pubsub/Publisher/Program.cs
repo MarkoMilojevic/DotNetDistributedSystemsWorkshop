00aafa214653028dd2725f624621494f1fb7b22f
Marko Milojevic <milojevicmarko92@gmail.com>
2021-12-11 14:52:18 +0100

    Implement Publisher.

--- pubsub/Publisher/Program.cs
+++ pubsub/Publisher/Program.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using EasyNetQ;
+using System;
 
 namespace Publisher
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        const string Amqp = "amqps://gsfihevq:eFtfoAM2d-JvSPxFBMu8te_VYbO91cBN@bunny.rmq.cloudamqp.com/gsfihevq";
+
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IBus bus = RabbitHutch.CreateBus(Amqp);
+
+            int count = 1;
+            while (true)
+            {
+                Console.WriteLine("Press a key to publish a message...");
+                Console.ReadKey(false);
+                string message = $"Message {count++} from {Environment.MachineName} at {DateTime.UtcNow:O}";
+                bus.PubSub.Publish(message);
+                Console.WriteLine($"Published message: {message}");
+            }
         }
     }
 }
