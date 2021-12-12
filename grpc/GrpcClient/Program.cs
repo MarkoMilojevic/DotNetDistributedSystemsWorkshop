4eae3965fc9d58a527b9070c9b40fccc0a132a6f
Marko Milojevic <milojevicmarko92@gmail.com>
2021-12-12 13:41:22 +0100

    Add grpc code.

---
+++ grpc/GrpcClient/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Grpc.Net.Client;
+using GrpcGreeting;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using GrpcChannel channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+            Greeter.GreeterClient client = new Greeter.GreeterClient(channel);
+
+            Console.WriteLine("Connected to GRPC endpoint. Press 1,2 or 3 to send a request:");
+
+            while (true)
+            {
+                int friendliness = Int32.Parse(Console.ReadKey().KeyChar.ToString());
+
+                Console.WriteLine();
+
+                HelloRequest request = new HelloRequest
+                {
+                    Name = "Dylan",
+                    Friendliness = friendliness,
+                    Language = "en"
+                };
+
+                HelloReply reply = await client.SayHelloAsync(request);
+
+                Console.WriteLine(reply.Message);
+            }
+
+        }
+    }
+}
